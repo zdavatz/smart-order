34136ea7be7efec153a8958c258dd2f187ae31d7
cybermax <cybrmx@gmail.com>
2017-07-17 18:41:31 +0200

    - fixed conflict in Pair utility class

--- app/models/Pair.java
+++ app/models/Pair.java
@@ -1,19 +1,14 @@
 package models;
 
 /**
-<<<<<<< HEAD
  * Implements a Pair class
  *
  * @param <T>
  * @param <U>
  * @author Max
  */
-public class Pair<T, U>{
-=======
- * Created by maxl on 10.03.2017.
- */
+
 public class Pair<T, U> {
->>>>>>> 509baf833fc39930508199a31e3b40637e78f6fb
     public final T first;
     public final U second;
 
