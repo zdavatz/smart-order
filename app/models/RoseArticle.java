a561ce7672121a186e777ed9b18274ee5b0a0c8f
zdavatz@gmail.com <zdavatz@gmail.com>
2020-09-25 11:52:56 +0200

    only show stock if stock<20

--- app/models/RoseArticle.java
+++ app/models/RoseArticle.java
@@ -263,7 +263,7 @@ public class RoseArticle {
         } else if (stock > 20) {
             this.stock_string = "> 20";
         } else {
-            this.stock_string = "nur noch " + stock + " Stück";
+            this.stock_string = stock;
         }
     }
 
